e19181a5045bd63b26ac1e92cb6fdaec60e3cf25
Aleksandr Veriasov <expert90210@gmail.com>
2025-09-09 16:03:09 +0200

    fix nginx.conf

--- gateway/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-: "${WEBHOOK_PORT:=8081}"
-
-envsubst '$WEBHOOK_PORT' \
-  < /etc/nginx/conf.d/default.conf.template \
-  > /etc/nginx/conf.d/default.conf
-
-exec nginx -g 'daemon off;'
